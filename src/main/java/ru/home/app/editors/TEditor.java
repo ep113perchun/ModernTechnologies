db8e174e56d1167b25e858f1bc64a7d49081f89c
Vladislav <vlad.perchun.93@gmail.com>
2024-10-18 18:03:54 +0700

    Initial commit

---
+++ src/main/java/ru/home/app/editors/TEditor.java
@@ -0,0 +1,11 @@
+package ru.home.app.editors;
+
+public interface TEditor {
+////    boolean numberIsZero();
+////    addSign();
+////    addNumber();
+////    addZero();
+////    slaughteringSymbol();
+////    clear();
+////    edit();
+}
---
+++ src/main/java/ru/home/app/editors/UEditor.java
@@ -0,0 +1,4 @@
+package ru.home.app.editors;
+
+public class UEditor {
+}
